6cdc6c0c4ce3db586cda0a3f94a9b063de748033
icaro56 <icarogoulart@yahoo.com.br>
2019-08-22 11:59:34 -0300

    Update Unity Atom Sample to canary branche

---
+++ Assets/Scripts/DangerTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms;
+using UnityEngine;
+
+public class DangerTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private ColliderGameObjectEvent _dangerEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _dangerEvent.Raise(other, other.gameObject);
+    }
+}
--- Assets/Scripts/DecreasePlayerHealth.cs
+++ Assets/Scripts/DecreasePlayerHealth.cs
@@ -1,14 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityAtoms;
+using UnityAtoms.Tags;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [CreateAssetMenu(menuName = "Unity Atoms/Examples/Intro/Game Actions/Decrease Player Health")]
 public class DecreasePlayerHealth : ColliderGameObjectAction
 {
+    [FormerlySerializedAs("TagPlayer")]
+    [SerializeField]
+    private StringConstant _tagPlayer;
+
+
     public override void Do(Collider collider, GameObject gameObject)
     {
-        if (collider.tag == "Player")
+        if (collider.gameObject.HasTag(_tagPlayer))
         {
             collider.GetComponent<PlayerHealth>().Health.Value -= 10;
         }
